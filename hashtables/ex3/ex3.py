7fa7920117a9b8f00fa0a2499b3e42f8ae8e3b04
roselandroche <r.landroche@gmail.com>
2020-08-14 20:52:19 -0400

    ex4 functional, test passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,8 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    count_dict = {}
 
     return result
 
@@ -15,3 +17,31 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+
+'''
+U
+Input
+    List of lists (up to 10 lists) containing integers
+Output
+    List of integers that exist in all given lists
+
+Task
+    Find any integers that exist in all lists
+    Save and return a list with just those integers
+    Order NOT important
+
+* RUNTIME COMPLEXITY VERY IMPORTANT
+
+P
+Create result list
+Use a for loop to separate list of lists? destructure?
+Create a dict
+    key: integer
+    value: count
+Iterate over lists
+    access key in dict
+    if value > 1
+        append to result list
+Return result
+'''
